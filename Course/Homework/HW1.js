9acfd72331da94ff43833f3ee37bb64686dadfca
July <106986983+July-vilh@users.noreply.github.com>
2024-09-24 21:45:13 +0300

    Update HW1.js

--- Course/Homework/HW1.js
+++ Course/Homework/HW1.js
@@ -1,6 +1,6 @@
-// 2. Написать функцию, которая принимает 2 строки ** 2. Write a function that takes 2 strings. **
-// В самой функции мы должны сделать сложение 2 строк (строки должны быть в виде чисел) ** Inside the function, we should add the two strings (the strings should represent numbers)** 
-// и посчитать их сумму использую конвертацию в число ** calculate their sum by converting them to numbers. **
+/* 2. Написать функцию, которая принимает 2 строки // 2. Write a function that takes 2 strings. 
+В самой функции мы должны сделать сложение 2 строк (строки должны быть в виде чисел) // Inside the function, we should add the two strings (the strings should represent numbers) 
+и посчитать их сумму использую конвертацию в число ** calculate their sum by converting them to numbers. */
 
 function sum(a, b){
     let aa = parseInt(a);
@@ -14,10 +14,9 @@ sum("3", "4")
 
 ____________
 
-// 3. Написать функцию, которая принимает 3 строки ** 3. Write a function that takes 3 strings **
-//В самой функции посчитать произведение 3 строк с учетом того, что их нужно конвертировать в число **  Inside the function, calculate the product of the 3 strings, 
-//making sure to convert them to numbers **
-//Важно: строка может иметь значение с плаваюшей точкой (parseFloat) ** Important: the string can contain a floating-point value (use parseFloat) **
+/* 3. Написать функцию, которая принимает 3 строки // 3. Write a function that takes 3 strings 
+В самой функции посчитать произведение 3 строк с учетом того, что их нужно конвертировать в число //  Inside the function, calculate the product of the 3 strings, making sure to convert them to numbers 
+Важно: строка может иметь значение с плаваюшей точкой (parseFloat) // Important: the string can contain a floating-point value (use parseFloat) */
 
 function multiply (a, b, c){
     aa = parseFloat(a);
