311ee595f807055a0e96ffafb95586d5363a3079
July <106986983+July-vilh@users.noreply.github.com>
2024-09-24 21:41:09 +0300

    Update HW1.js

--- Course/Homework/HW1.js
+++ Course/Homework/HW1.js
@@ -11,3 +11,18 @@ function sum(a, b){
 sum("3", "4")
 
 //Result: 7 (as expected)
+
+____________
+
+// 3. Написать функцию, которая принимает 3 строки ** 3. Write a function that takes 3 strings **
+//В самой функции посчитать произведение 3 строк с учетом того, что их нужно конвертировать в число **  Inside the function, calculate the product of the 3 strings, 
+//making sure to convert them to numbers **
+//Важно: строка может иметь значение с плаваюшей точкой (parseFloat) ** Important: the string can contain a floating-point value (use parseFloat) **
+
+function multiply (a, b, c){
+    aa = parseFloat(a);
+    bb = parseInt(b);
+    cc = parseInt(c);
+    console.log(aa * bb * cc)
+}
+multiply("3.14", "10", "5")
